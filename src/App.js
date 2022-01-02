10b6bd08fa36376c8de42ccc218de9289055d403
Rachel Napier <napier.rachel93@gmail.com>
2022-01-02 16:51:31 -0600

    h1 and h3 edit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function App() {
   return (
     <>
-      <h1>Welcome to react portfolio Project</h1>
-      <h3>-Shaif Arfan</h3>
+      <h1>Welcome to my personal portfolio</h1>
+      <h3>-Rachel Napier</h3>
     </>
   );
 }
