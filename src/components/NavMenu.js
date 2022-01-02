aa5c0881a822bc945da89c7e426ef7d742e9c757
Rachel Napier <napier.rachel93@gmail.com>
2022-01-02 17:51:26 -0600

    Imported NavMenu enclosing an Unordered list.

---
+++ src/components/NavMenu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function NavMenu() {
+  return (
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
