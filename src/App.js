811babd6af94a04ebc90b3f66ad9a12a74c71c95
Rachel Napier <napier.rachel93@gmail.com>
2022-01-02 20:48:32 -0600

    Imported pages.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import NavMenu from './components/NavMenu';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
 
 export default function App() {
   return (
     <>
       <Router>
         <NavMenu />
+        <Home />
+        <About />
+        <Projects />
+        <Contact />
       </Router>
     </>
   );
