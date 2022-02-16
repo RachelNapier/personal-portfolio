9f4020b82c4c6396836d20127733a13fceb7dbe1
Rachel Napier <napier.rachel93@gmail.com>
2022-02-16 11:06:10 -0600

    Styled and centered navigation

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavMenuStyles = styled.div`
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 1rem 0;
+  background: var(--dark-bg);
+
+  ul {
+    text-align: center;
+    max-width: 1200px;
+    margin: 0 auto;
+    width: 90%;
+
+    li {
+      display: inline-block;
+      border-radius: 8px;
+      transition: 0.3s easy background-color;
+      &:hover {
+        background-color: var(--tertiary-color);
+      }
+      a {
+        display: inline-block;
+        font-family: 'RobotoMono Regular';
+        padding: 1rem 2rem;
+        font-size: 2rem;
+        color: var(--primary-color-1);
+        outline: none;
+      }
+    }
+  }
+`;
 
 export default function NavMenu() {
   return (
-    <div>
+    <NavMenuStyles>
       <ul>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/" exact>
+            Home
+          </NavLink>
         </li>
         <li>
           <NavLink to="/about">About</NavLink>
@@ -18,6 +55,6 @@ export default function NavMenu() {
           <NavLink to="/contact">Contact</NavLink>
         </li>
       </ul>
-    </div>
+    </NavMenuStyles>
   );
 }
